3f938c3acf3fae31cbbdf4cdeb2d01905181feb4
Vishal Kumar <vishalkr00700@gmail.com>
2025-05-08 19:13:25 +0530

    Solving netlify issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import AboutUs from "./Pages/AboutUs";
-import ContactUs from "./Pages/ContactUs";
-import Services from "./Pages/Services";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import "./App.css";
-import { useRef } from "react";
 
 function App() {
 
